1f304e5fd00c37444986664db7e8df116771cae3
TranTheTien <107165467+TranTheTien@users.noreply.github.com>
2022-10-21 08:53:13 +0700

    hello

---
+++ models/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+//const url = "mongodb://localhost:27017/EmployeeDB";
+//const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
+
+const url = "mongodb+srv://minhnghiax:112233as@cluster0.cngpz.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(url, { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log("MongoDB Connection Succeeded"); } else {
+        console.log("An Error Occured");
+    }
+})
+
+require('./toy.model');
